2d8b2dd58febe33f2f3fec45571c95e209d94d66
nourinjh <nourin.abdelhadi@gmail.com>
2020-01-18 14:31:11 -0500

    formatting

--- js/camera.js
+++ js/camera.js
@@ -1,45 +1,46 @@
 //import database.js;
-function start(){
-    
-    //button toggle
-    var x = document.getElementById('camera');
-    if(x.style.display === "none"){
-        x.style.display = "block";
-        document.getElementById("scan").value="Turn Off Camera";
-    }else{
-        x.style.display = "none";
-        document.getElementById("scan").value="Scan Now";
-    }
+function start() {
+  //button toggle
+  var x = document.getElementById("camera");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    document.getElementById("scan").value = "Turn Off Camera";
+  } else {
+    x.style.display = "none";
+    document.getElementById("scan").value = "Scan Now";
+  }
 
-    //start of camera code
-    Quagga.init({
-        inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: document.querySelector('#camera')    // Or '#yourElement' (optional)
-        },
-        decoder: {
-            readers: ["ean_reader"]
-            /* reader options:
+  //start of camera code
+  Quagga.init(
+    {
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector("#camera") // Or '#yourElement' (optional)
+      },
+      decoder: {
+        readers: ["ean_reader"]
+        /* reader options:
             "code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader",
             "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader",
             "2of5_reader", "code_93_reader"
             */
-        }
-    }, function (err) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start(); 
-    });
+      }
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+    }
+  );
 
-    Quagga.onDetected(function (data) {
-        console.log(data.codeResult.code);
-        document.querySelector('#result').innerText = data.codeResult.code;
-        var barcode = data.codeResult.code;
-        //var barList = []
-    });
-
+  Quagga.onDetected(function(data) {
+    console.log(data.codeResult.code);
+    document.querySelector("#result").innerText = data.codeResult.code;
+    var barcode = data.codeResult.code;
+    //var barList = []
+  });
 }
